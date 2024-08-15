d3cd18ec680dbc8dae55fc7f997751c1b43f2d1b
speregil <s.gilparga@gmail.com>
2024-08-15 13:25:15 +1200

    trying to fix the client ar plane detection

--- Assets/Scripts/AR/TrackingManager.cs
+++ Assets/Scripts/AR/TrackingManager.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
 
-public class TrackingManager : MonoBehaviour
+public class TrackingManager : NetworkBehaviour
 {
     private GameObject trackables;
+    private ARPlaneManager planeManager;
 
     // Start is called before the first frame update
     void Start()
     {
         trackables = transform.Find("Trackables").gameObject;
+        planeManager = gameObject.GetComponent<ARPlaneManager>();
+
+        if(IsClient)
+            planeManager.enabled = false;
     }
 
     public void CleanTrackables()
